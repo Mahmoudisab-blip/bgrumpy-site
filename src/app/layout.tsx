6bbdc40b200aff90d23b52dea5bc30b37f395a72
Mahmoudisab-blip <mahmoudi.sab@gmail.com>
2026-04-18 14:46:38 +0200

    auto save 2026-04-18 14:46:38

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Navbar from "@/src/components/Navbar";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -14,7 +15,10 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body>
-        <div className="min-h-screen">{children}</div>
+        <div className="min-h-screen pb-[calc(7rem_+_env(safe-area-inset-bottom))]">
+          {children}
+        </div>
+        <Navbar />
       </body>
     </html>
   );
